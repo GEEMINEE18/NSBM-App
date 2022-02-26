c3846bf0bc0dbc5aa9a3576dd4f606ed6f7c3160
ovinransika <ovinransika9172@gmail.com>
2022-02-26 14:55:26 +0530

    Added the navigation drawer with a toggle button on the action bar

--- app/src/main/java/com/example/mygreenapp/MainActivity.kt
+++ app/src/main/java/com/example/mygreenapp/MainActivity.kt
@@ -3,12 +3,40 @@ package com.example.mygreenapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Gives the ability open the drawer by the Toggle button
+    //lateinit: will initialize later
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //For the drawerLayout to work we should include: id 'kotlin-android-extensions' in build.gradle(Module) plugins section
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miItem1 -> Toast.makeText(applicationContext,
+                    "Clicked Item 1",Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext,
+                    "Clicked Item 2",Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext,
+                    "Clicked Item 3",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
 
         val button = findViewById<Button>(R.id.clubSocietyBtn)
         val newsBtn = findViewById<Button>(R.id.newsBtn)
@@ -22,5 +50,12 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent (this,NewsPageActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
