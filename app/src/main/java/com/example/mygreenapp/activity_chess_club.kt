05ac388d538aff6d3dd9c85478f0699902e782f3
GEEMINEE18 <gpbliyanage@students.nsbm.ac.lk>
2022-02-26 20:47:05 +0530

    create sports club register forms

---
+++ app/src/main/java/com/example/mygreenapp/activity_chess_club.kt
@@ -0,0 +1,11 @@
+package com.example.mygreenapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_chess_club : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chess_club)
+    }
+}
