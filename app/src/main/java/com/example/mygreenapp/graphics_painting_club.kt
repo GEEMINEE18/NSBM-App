fac0a4172b2865c958aa51c854c4c50b2117b0c0
GEEMINEE18 <gpbliyanage@students.nsbm.ac.lk>
2022-03-16 20:45:55 +0530

    create activity based clubs UI

--- app/src/main/java/com/example/mygreenapp/graphics_painting_club.kt
+++ app/src/main/java/com/example/mygreenapp/graphics_painting_club.kt
@@ -3,9 +3,9 @@ package com.example.mygreenapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class bottom_nav : AppCompatActivity() {
+class graphics_painting_club : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_nav)
+        setContentView(R.layout.activity_graphics_painting_club)
     }
 }
