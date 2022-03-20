7260c044c60f06e00b9ffe9819050a959134fa32
GEEMINEE18 <gpbliyanage@students.nsbm.ac.lk>
2022-03-20 12:51:17 +0530

    create activity based clubs register forms UI

---
+++ app/src/main/java/com/example/mygreenapp/volllyball_club.kt
@@ -0,0 +1,11 @@
+package com.example.mygreenapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class volllyball_club : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_volllyball_club)
+    }
+}
