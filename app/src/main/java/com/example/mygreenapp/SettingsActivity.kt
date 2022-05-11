11bf81377bdc4e2c1e8a7e54e2fc1e03f7e09101
ovinransika <ovinransika9172@gmail.com>
2022-05-11 21:22:29 +0530

    - Added Dark Mode toggle switch to SettingsActivity.kt

--- app/src/main/java/com/example/mygreenapp/SettingsActivity.kt
+++ app/src/main/java/com/example/mygreenapp/SettingsActivity.kt
@@ -2,10 +2,22 @@ package com.example.mygreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.mygreenapp.databinding.ActivityAddMeetingBinding
+import com.example.mygreenapp.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.darkSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
